a7c1069f7953a3131aa51ce04d7011be34a60448
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-12 17:51:38 +0530

    Check if a Number is Odd or Even

--- programs.js
+++ programs.js
@@ -1,35 +1,17 @@
 
 
-// program to check if a number is prime or not
 
+
+// program to check if the number is even or odd
 // take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+const number = prompt("Enter a number: ");
 
-// check if number is equal to 1
-if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
 }
 
-// check if number is greater than 1
-else if (number > 1) {
-
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}
-
-// check if number is less than 1
+// if the number is odd
 else {
-    console.log("The number is not a prime number.");
+    console.log("The number is odd.");
 }
