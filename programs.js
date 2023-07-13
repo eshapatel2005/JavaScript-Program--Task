4ba8ef58af5481f8638df0347b016c827a1eddf4
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-13 18:23:21 +0530

    Print Prime Numbers in an Interval

--- programs.js
+++ programs.js
@@ -2,18 +2,28 @@
 
 
 
-// program to check leap year
-function checkLeapYear(year) {
+// program to print prime numbers between the two numbers
 
-  //three conditions to find out the leap year
-  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-      console.log(year + ' is a leap year');
-  } else {
-      console.log(year + ' is not a leap year');
-  }
+// take input from the user
+const lowerNumber = parseInt(prompt('Enter lower number: '));
+const higherNumber = parseInt(prompt('Enter higher number: '));
+
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+// looping from lowerNumber to higherNumber
+for (let i = lowerNumber; i <= higherNumber; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
 }
-
-// take input
-const year = prompt('Enter a year:');
-
-checkLeapYear(year);
