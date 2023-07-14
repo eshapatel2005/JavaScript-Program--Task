f617a836e4637a986d9efbeabf4c09b208205e06
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-14 18:32:22 +0530

    Check Armstrong Number

--- programs.js
+++ programs.js
@@ -2,17 +2,26 @@
 
 
 
-// program to generate a multiplication table
+// program to check an Armstrong number of three digits
 
-// take input from the user
-const number = parseInt(prompt('Enter an integer: '));
+let sum = 0;
+const number = prompt('Enter a three-digit positive integer: ');
 
-//creating a multiplication table
-for(let i = 1; i <= 10; i++) {
+// create a temporary variable
+let temp = number;
+while (temp > 0) {
+    // finding the one's digit
+    let remainder = temp % 10;
 
-    // multiply i with number
-    const result = i * number;
+    sum += remainder * remainder * remainder;
 
-    // display the result
-    console.log(`${number} * ${i} = ${result}`);
+    // removing last digit from the number
+    temp = parseInt(temp / 10); // convert float into integer
+}
+// check the condition
+if (sum == number) {
+    console.log(`${number} is an Armstrong number`);
+}
+else {
+    console.log(`${number} is not an Armstrong number.`);
 }
