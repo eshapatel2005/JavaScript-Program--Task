0952006bc576d23983fef8ce2ffc2bcd48a38988
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-16 17:41:42 +0530

    Program to Find the Largest Among Three Numbers

--- programs.js
+++ programs.js
@@ -2,6 +2,24 @@
 
 
 
-// generating  a random number
-const a = Math.random();
-console.log(a);
+// program to find the largest among three numbers
+
+// take input from the user
+let num1 = parseFloat(prompt("Enter first number: "));
+let num2 = parseFloat(prompt("Enter second number: "));
+let num3 = parseFloat(prompt("Enter third number: "));
+let largest;
+
+// check the condition
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+
+// display the result
+console.log("The largest number is " + largest);
