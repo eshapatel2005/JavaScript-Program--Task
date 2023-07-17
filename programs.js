0663f4795a1770ae725d13c337bbd2a846abdbf6
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-17 17:56:38 +0530

    Program to Make a Simple Calculator

--- programs.js
+++ programs.js
@@ -2,17 +2,30 @@
 
 
 
-// program to generate fibonacci series up to n terms
+// program for a simple calculator
 
-// take input from the user
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-console.log('Fibonacci Series:');
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
 
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
 }
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
