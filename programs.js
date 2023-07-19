ed1d214a2a194a857e718b466f6a8e9cada10d62
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-19 18:19:54 +0530

    Program to Check Whether a String is Palindrome or Not

--- programs.js
+++ programs.js
@@ -1,11 +1,29 @@
 
 
-// program to find the ASCII value of a character
 
-// take input from the user
-let string = prompt('Enter a character: ');
 
-// convert into ASCII value
-let result = string.charCodeAt(0);
+// program to check if the string is palindrome or not
 
-console.log(`The ASCII value is: ${result}`);
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+// take input
+let string = prompt('Enter a string: ');
+
+// call the function
+let value = checkPalindrome(string);
+
+console.log(value);
