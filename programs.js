92f2e7801be2baf2af005312f6b086f2e0cc404e
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-19 18:13:09 +0530

    Program to Display Fibonacci Sequence Using Recursion

--- programs.js
+++ programs.js
@@ -1,45 +1,27 @@
-// program to shuffle the deck of cards
 
-// declare card elements
-const suits = ["Spades", "Diamonds", "Club", "Heart"];
-const values = [
-  "Ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King",
-];
 
-// empty array to contain cards
-let deck = [];
 
-// create a deck of cards
-for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-        let card = { Value: values[x], Suit: suits[i] };
-        deck.push(card);
+
+
+
+// program to display fibonacci sequence using recursion
+function fibonacci(num) {
+    if(num < 2) {
+        return num;
+    }
+    else {
+        return fibonacci(num-1) + fibonacci(num - 2);
     }
 }
 
-// shuffle the cards
-for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let temp = deck[i];
-    deck[i] = deck[j];
-    deck[j] = temp;
+// take nth term input from the user
+const nTerms = prompt('Enter the number of terms: ');
+
+if(nTerms <=0) {
+    console.log('Enter a positive integer.');
 }
-
-console.log('The first five cards are:');
-
-// display 5 results
-for (let i = 0; i < 5; i++) {
-    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+else {
+    for(let i = 0; i < nTerms; i++) {
+        console.log(fibonacci(i));
+    }
 }
