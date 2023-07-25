e2d0099b480b269a620cf31b69737c0c4249975f
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-25 17:56:16 +0530

    Program to Generate Random String

--- programs.js
+++ programs.js
@@ -2,10 +2,19 @@
 
 
 
-// program to format numbers as currency string
-let formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-});
+// program to generate random strings
 
-formatter.format(2500);
+// declare all characters
+let characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+console.log(generateString(5));
