8af0f71f13103d0b10e79c00e96f51c70fb6a27d
eshapatel2005 <esha.patel5605@gmail.com>
2023-07-30 18:26:08 +0530

    Program to Check if An Object is An Array

--- programs.js
+++ programs.js
@@ -2,21 +2,23 @@
 
 
 
-// program to append an object to an array
+// program to check if an object is an array
 
-function insertObject(arr, obj) {
+function checkObject(arr) {
 
-    // append object
-    arr.push(obj);
-    
-    console.log(arr);
+    // check if arr is array
+    const result = Array.isArray(arr);
+
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+
 }
 
-// original array
 let array = [1, 2, 3];
 
-// object to add
-let object = {x: 12, y: 8};
-
 // call the function
-insertObject(array, object);
+checkObject(array);
