dce90667f37234d6a147e6fbbc57bea482000741
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-01 17:32:16 +0530

    Program to Merge Two Arrays and Remove Duplicate Items

--- programs.js
+++ programs.js
@@ -1,10 +1,16 @@
 
-// program to remove duplicate value from an array
 
-function getUnique(arr){
-    
+
+
+
+// program to merge and remove duplicate value from an array
+
+function getUniqueAfterMerge(arr1, arr2){
+
+    // merge two arrays
+    let arr = arr1.concat(arr2);
     let uniqueArr = [];
-    
+
     // loop through array
     for(let i of arr) {
         if(uniqueArr.indexOf(i) === -1) {
@@ -14,8 +20,9 @@ function getUnique(arr){
     console.log(uniqueArr);
 }
 
-let array = [1, 2, 3, 2, 3];
+let array1 = [1, 2, 3];
+let array2 = [2, 3, 5]
 
 // calling the function
 // passing array argument
-getUnique(array);
+getUniqueAfterMerge(array1, array2);
