3abacd418026e98f1fbf8dfde39fbc5e32ed6ca5
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-03 17:48:48 +0530

    program to perform intersection between two arrays using Set

--- programs.js
+++ programs.js
@@ -6,11 +6,11 @@
 // program to perform intersection between two arrays using Set
 // intersection contains the elements of array1 that are also in array2
 
-function performIntersection(arr1, arr2) {
+function performIntersection(arr1, arr3) {
 
     // converting into Set
     let setA = new Set(arr1);
-    let setB = new Set(arr2);
+    let setB = new Set(arr3);
 
     let intersectionResult = [];
 
@@ -29,5 +29,5 @@ function performIntersection(arr1, arr2) {
 let array1 = [1, 2, 3, 5, 9];
 let array2 = [1, 3, 5, 8];
 
-let result = performIntersection(array1, array2);
+let result = performIntersection(array1, array3);
 console.log(result);
