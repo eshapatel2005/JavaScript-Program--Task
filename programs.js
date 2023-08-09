0306ea51a2e98fb8205fcfb0172c33eb2ba69e29
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-09 20:30:17 +0530

    Program to Pass a Function as Parameter

--- programs.js
+++ programs.js
@@ -3,28 +3,22 @@
 
 
 
-// program to check if a number is a float or integer value
+// program to pass a function as a parameter
 
-function checkNumber(x) {
-
-    // check if the passed value is a number
-    if(typeof x == 'number' && !isNaN(x)){
-    
-        // check if it is integer
-        if (Number.isInteger(x)) {
-            console.log(`${x} is integer.`);
-        }
-        else {
-            console.log(`${x} is a float value.`);
-        }
-    
-    } else {
-        console.log(`${x} is not a number`);
-    }
+function greet() {
+    return 'Hello';
 }
 
-checkNumber('hello');
-checkNumber(44);
-checkNumber(3.4);
-checkNumber(-3.4);
-checkNumber(NaN);
+// passing function greet() as a parameter
+function name(user, func)
+{
+
+    // accessing passed function
+    const message = func();
+
+    console.log(`${message} ${user}`);
+}
+
+name('John', greet);
+name('Jack', greet);
+name('Sara', greet);
