6a745e63d4342c66134f5b4ed3a2fde651ac303c
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-10 19:35:50 +0530

    Program to Convert Date to Number

--- programs.js
+++ programs.js
@@ -2,32 +2,11 @@
 
 
 
+// program to convert date to number
+// create date
+let d1 = new Date();
+console.log(d1);
 
-// program to write to console
-
-// passing number 
-console.log(8);
-
-// passing string
-console.log('HELLO');
-
-// passing variable
-let x = 'HELLO';
-console.log(x);
-
-// passing function
-function sayName() {
-    return 'HELLO FRIENDS';
-}
-console.log(sayName());
-
-// passing string and a variable
-let name = 'FRIENDS';
-console.log('Hello ' + name);
-
-// passing object
-let obj = {
-    name: 'FRIENDS',
-    age: 28
-}
-console.log(obj);
+// converting to number
+let result = d1.getTime();
+console.log(result);
