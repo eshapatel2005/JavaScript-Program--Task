389380497ac291c62c1390ee37361558698d6402
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-10 19:34:57 +0530

    Program to Write to Console

--- programs.js
+++ programs.js
@@ -3,10 +3,31 @@
 
 
 
-// program to trim a string
+// program to write to console
 
-let string = '      Hello World       ';
+// passing number 
+console.log(8);
 
-let result = string.split(' ').join('');
+// passing string
+console.log('HELLO');
 
-console.log(result);
+// passing variable
+let x = 'HELLO';
+console.log(x);
+
+// passing function
+function sayName() {
+    return 'HELLO FRIENDS';
+}
+console.log(sayName());
+
+// passing string and a variable
+let name = 'FRIENDS';
+console.log('Hello ' + name);
+
+// passing object
+let obj = {
+    name: 'FRIENDS',
+    age: 28
+}
+console.log(obj);
