0c4f3de3bf2a961ec81158f7d6137f492ee623d4
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-12 17:06:47 +0530

    Program to Add Digits of a Number

--- programs.js
+++ programs.js
@@ -2,33 +2,11 @@
 
 
 
-// program to perform function overloading
-
-function sum() {  
-    
-    // if no argument        
-    if (arguments.length == 0) {  
-        console.log('You have not passed any argument');  
-    }
-
-    // if only one argument 
-    else if (arguments.length == 1) {  
-        console.log('Pass at least two arguments');  
-    }
-
-    // multiple arguments
-    else {
-        let result = 0;
-        let length = arguments.length;
-    
-        for (i = 0; i < length; i++) {  
-            result = result + arguments[i];  
-        }  
-        console.log(result); 
-    }  
+var num=1234, rem, sum=0;
+while(num)
+{
+  rem = num%10;
+  sum = sum+rem;
+  num = Math.floor(num/10);
 }
-
-sum();
-sum(5); 
-sum(5, 9);    
-sum(1, 2, 3, 4, 5, 6, 7, 8, 9); 
+document.write(sum);
