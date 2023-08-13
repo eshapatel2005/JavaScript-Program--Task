a6bb948faf04bd0075d6a3124e45adf17281cc78
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-13 20:51:27 +0530

    Input value from the user using prompt

--- programs.js
+++ programs.js
@@ -2,11 +2,7 @@
 
 
 
-var num=1234, rem, sum=0;
-while(num)
-{
-  rem = num%10;
-  sum = sum+rem;
-  num = Math.floor(num/10);
-}
-document.write(sum);
+var name = prompt("Enter Your name:");
+var msg = "Welcome "+name;
+//alert(msg);
+document.write(msg);
