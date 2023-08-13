7ab4affa980283ca8b187fa655ab6ae212782a63
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-13 20:54:10 +0530

    Program to Calculate the Area of a Triangle

--- programs.js
+++ programs.js
@@ -2,7 +2,14 @@
 
 
 
-var name = prompt("Enter Your name:");
-var msg = "Welcome "+name;
-//alert(msg);
-document.write(msg);
+
+
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the area
+let areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
