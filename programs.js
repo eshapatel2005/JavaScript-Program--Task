e518719bceaacc2b44e088d4a6e9927e11924528
eshapatel2005 <esha.patel5605@gmail.com>
2023-08-28 18:10:09 +0530

    Program to finding the largest number in an array

--- programs.js
+++ programs.js
@@ -3,12 +3,12 @@
 
 
 
-var array = [3 , 6, 2, 56, 32, 5, 89, 32];
-var largest= 0;
+let array = [3 , 6, 2, 56, 32, 5, 89, 32];
+let largest= 0;
 
 for (i=0; i<=largest;i++){
     if (array>largest) {
-        var largest=array[i];
+        let largest=array[i];
     }
 }
 
