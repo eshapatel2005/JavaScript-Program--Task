6faec9423fbf6596103cc4fd89f509e11327b7ce
eshapatel2005 <esha.patel5605@gmail.com>
2023-09-13 18:44:49 +0530

    Program to Add Two Numbers

--- programs.js
+++ programs.js
@@ -1,40 +1,10 @@
 
 
-// program to solve quadratic equation
-let root1, root2;
+let num1 = 5;
+let num2 = 3;
 
-// take input from the user
-let a = prompt("Enter the first numbers: ");
-let b = prompt("Enter the second numbers: ");
-let c = prompt("Enter the third numbers: ");
+// add two numbers
+let sum = num1 + num2;
 
-// calculate discriminant
-let discriminant = b * b - 4 * a * c;
-
-// condition for real and different roots
-if (discriminant > 0) {
-    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-
-    // result
-    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-}
-
-// condition for real and equal roots
-else if (discriminant == 0) {
-    root1 = root2 = -b / (2 * a);
-
-    // result
-    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-}
-
-// if roots are not real
-else {
-    let realPart = (-b / (2 * a)).toFixed(2);
-    let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
-
-    // result
-    console.log(
-    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
-  );
-}
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
